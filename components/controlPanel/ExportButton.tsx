932059589423eef05cfe280dbdaf28bab94a00b3
harsh661 <plasmaharsh@gmail.com>
2023-09-02 16:11:26 +0530

    feat: add download functionality

--- components/controlPanel/ExportButton.tsx
+++ components/controlPanel/ExportButton.tsx
@@ -11,6 +11,7 @@ import { DropdownMenuTrigger } from "@radix-ui/react-dropdown-menu"
 import { Button } from "../ui/button"
 import { useGetPreference } from "@/app/contexts/PreferenceContext"
 import { useToast } from "../ui/use-toast"
+import { toPng, toSvg } from "html-to-image"
 
 interface ExportButtonProps {
   targetRef: MutableRefObject<HTMLElement | null>
@@ -33,14 +34,34 @@ const ExportButton: React.FC<ExportButtonProps> = ({ targetRef }) => {
     toast({ title: "Link copied to clipboard", className: "dark", duration: 3000})
   }
 
+  const downloadImage = async (name: string, format: string) => {
+    let imageUrl, filename
+    switch(format) {
+        case "PNG":
+            imageUrl = await toPng(targetRef.current as HTMLElement, {pixelRatio: 2})
+            filename = `${name}.png`
+            break
+        case "SVG":
+            imageUrl = await toSvg(targetRef.current as HTMLElement, {pixelRatio: 2})
+            filename = `${name}.svg`
+            break
+        default:
+            return
+    }
+    const a = document.createElement("a")
+    a.href = imageUrl
+    a.download = filename
+    a.click()
+  }
+
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
         <Button className="my-auto">Export</Button>
       </DropdownMenuTrigger>
       <DropdownMenuContent className="dark">
-        <DropdownMenuItem>Save PNG</DropdownMenuItem>
-        <DropdownMenuItem>Save SVG</DropdownMenuItem>
+        <DropdownMenuItem onClick={() => downloadImage(preferences.title, "PNG")}>Save PNG</DropdownMenuItem>
+        <DropdownMenuItem onClick={() => downloadImage(preferences.title, "SVG")}>Save SVG</DropdownMenuItem>
         <DropdownMenuItem onClick={copyLink}>Copy Link</DropdownMenuItem>
       </DropdownMenuContent>
     </DropdownMenu>
